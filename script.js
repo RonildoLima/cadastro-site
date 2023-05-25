b915d76013c8c76335ee3a55bedd25d05b9e6121
RonildoLima <80013701+RonildoLima@users.noreply.github.com>
2023-05-24 22:57:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function cadastrar() {
   };
 
   // Enviar a solicitação POST usando a API Fetch
-  fetch('https://voluble-travesseiro-3e9e2f.netlify.app', {
+  fetch('http://localhost:3000/cadastrar', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
